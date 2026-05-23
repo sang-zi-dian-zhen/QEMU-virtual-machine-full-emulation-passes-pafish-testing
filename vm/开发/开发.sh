e653b854f7614f9dd235c6f33415a50798186949
桑梓店镇 <wanmeics1980@qq.com>
2026-05-24 01:43:43 +0800

    Add files via upload

--- vm/开发/开发.sh
+++ vm/开发/开发.sh
@@ -1,1 +1,50 @@
+#!/bin/bash
+
+#配置工作目录	#是=1	否=3
+work_dir=1
+
+##	##	##	##	##	##	##	##	##	##	##	##	##	##	
+
+if [ $work_dir == 1 ]; then
+mkdir -p ~/公共/开发[QEMU]	~/公共/开发[QEMU]/diff	~/公共/开发[QEMU]/diff/a	~/公共/开发[QEMU]/diff/b
+
+tar -xf qemu-10.2.2.tar.xz -C ~/公共/开发[QEMU]/diff
+cd ~/公共/开发[QEMU]/diff/qemu-10.2.2;patch -p1 < ../../../../qemu-10.2.2.patch
+
+rm -rf ~/公共/开发[QEMU]/QEMU开发
+cp -r ~/公共/开发[QEMU]/diff/qemu-10.2.2	~/公共/开发[QEMU]/QEMU开发
+fi
+
+##	##	##	##	##	##	##	##	##	##	##	##	##	##	
+
+
+my_array=("
+
+/hw/i386/acpi-build.c
+" "
+/hw/acpi/aml-build.c
+" "
+/include/hw/acpi/aml-build.h
+" "
+/hw/smbios/smbios.c
+" "
+/include/hw/firmware/smbios.h
+" "
+/hw/i386/fw_cfg.c
+
+")
+ 
+
+for item in "${my_array[@]}"; do
+item=$(echo $item | sed  's/\n//g')
+
+install -D ~/公共/开发[QEMU]/diff/qemu-10.2.2$item	~/公共/开发[QEMU]/diff/a$item
+install -D ~/公共/开发[QEMU]/QEMU开发$item	~/公共/开发[QEMU]/diff/b$item
+
+done;	diff -uprN ~/公共/开发[QEMU]/diff/a ~/公共/开发[QEMU]/diff/b | 
+sed -e 's/\/home\/.*\[QEMU\]\/diff\/a/a/g' -e 's/\/home\/.*\[QEMU\]\/diff\/b/b/g' > ~/公共/开发[QEMU]/开发补丁[$(date "+%Y-%m-%d_%H:%M:%S")].patch
+
+
+
+
 
